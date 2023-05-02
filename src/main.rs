2da0ba2b23078b4985b3f6a4dfb38fef30261a18
bf39L <boliu1234@gmail.com>
2023-05-02 17:48:22 +1200

    code clean up
    - extract mouse move detect into a function
    - add function to revers cursor to origin location

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
-use std::{thread, time, env};
 use chrono::Local;
-use mouse_rs::{Mouse};
+use mouse_rs::types::Point;
+use mouse_rs::Mouse;
 use rdev::{simulate, EventType, Key, SimulateError};
+use std::{env, thread, time};
 
-const ONE_MIN: time::Duration= time::Duration::from_secs(60);
+const ONE_MIN: time::Duration = time::Duration::from_secs(60);
 // const ONE_SEC: time::Duration= time::Duration::from_secs(1);
-const MILISEC_100: time::Duration= time::Duration::from_millis(100);
+const MILISEC_100: time::Duration = time::Duration::from_millis(100);
 const TIME_FORMAT: &str = "%Y-%M-%d %H:%M:%S";
 
 fn main() {
     let wait_time = get_wait_time();
-    println!("Stay awake at {}", Local::now().format(TIME_FORMAT) );
+    println!("Stay awake at {}", Local::now().format(TIME_FORMAT));
     println!("Each interval will sleep for {} min", wait_time);
     println!("################");
 
     let mousers = Mouse::new();
 
     let mut interval = 1;
-    // let init_pos = 0;
-    let fixed_width = 800;
+    let steps = 200;
     let multi = 4;
     let keypress_total = 3;
 
@@ -32,46 +32,56 @@ fn main() {
 
         println!("Moving...");
 
-        let (cur_x, cur_y) = match mousers.get_position() {
+        let (init_x, init_y) = match mousers.get_position() {
             Ok(mouse_pos) => (mouse_pos.x, mouse_pos.y),
-            Err(_) => (0,0),
+            Err(_) => (0, 0),
         };
-        let mut last_x = cur_x;
-        let mut last_y = cur_y;
-        for i in 0..(fixed_width/multi) {
+        let mut last_x = init_x;
+        let mut last_y = init_y;
+        let mut reversed = false;
+        let mut c = 0;
+        let mut direction = 1;
+        for i in 0..steps {
             thread::sleep(MILISEC_100);
-            // println!("{}, {}",(cur_x + i * multi) % fixed_width, (cur_y + i * multi) % fixed_width);
-            let (x, y) = match mousers.get_position() {
-                Ok(mouse_pos) => (mouse_pos.x, mouse_pos.y),
-                Err(_) => (0,0),
-            };
-            if last_x != x || last_y != y {
+
+            // println!("c: {}, x: {}, y: {}", c, (init_x + c * multi), (init_y + c * multi));
+            if detect_mouse_moved(
+                &mousers,
+                Point {
+                    x: last_x,
+                    y: last_y,
+                },
+            ) {
                 println!("Mouse moved, skip moving...");
                 break;
             }
 
             send(&EventType::MouseMove {
-                x: f64::from((cur_x + i * multi) % fixed_width),
-                y: f64::from((cur_y + i * multi) % fixed_width)
+                x: f64::from(init_x + c * multi),
+                y: f64::from(init_y + c * multi),
             });
 
-            last_x = (cur_x + i * multi) % fixed_width;
-            last_y = (cur_y + i * multi) % fixed_width
+            last_x = init_x + c * multi;
+            last_y = init_y + c * multi;
 
-            // enigo.mouse_move_to(
-            //     (cur_x + i * multi) % fixed_width,
-            //     (cur_y + i * multi) % fixed_width
-            // )
+            if i >= steps / 2 - 1 && !reversed {
+                println!("reversing...");
+                reversed = true;
+                direction = direction * -1;
+            }
+            c = c + direction;
         }
 
         for i in 0..keypress_total {
-            // enigo.key_click(enigo::Key::Shift);
             send(&EventType::KeyPress(Key::ShiftLeft));
-
-            println!("Key pressed {}/{}", i+1,keypress_total);
+            println!("Key pressed {}/{}", i + 1, keypress_total);
         }
 
-        println!("Movement {} made at {}", interval, Local::now().format(TIME_FORMAT));
+        println!(
+            "Movement {} made at {}",
+            interval,
+            Local::now().format(TIME_FORMAT)
+        );
         println!("================");
         interval += 1;
     }
@@ -79,12 +89,25 @@ fn main() {
 
 fn get_wait_time() -> i32 {
     let args: Vec<String> = env::args().collect();
-    let mut wait_time :i32 = 3;
+    let mut wait_time: i32 = 3;
     if args.len() >= 2 {
         wait_time = i32::from_str_radix(&args[1], 10).unwrap_or(wait_time);
     }
 
     return wait_time;
+}
+
+fn detect_mouse_moved(mousers: &Mouse, pos: Point) -> bool {
+    let (x, y) = match mousers.get_position() {
+        Ok(mouse_pos) => (mouse_pos.x, mouse_pos.y),
+        Err(_) => (0, 0),
+    };
+
+    if pos.x != x || pos.y != y {
+        return true;
+    }
+
+    return false;
 }
 
 fn send(event_type: &EventType) {
